d03b35b33f662dc319c7059273132f37a7202f62
Tiago Almeida <2025a081883@aerp.pt>
2024-12-09 10:43:01 +0000

    corrigir informação do forminsc

--- RoboticsCodeRaulWeb/resources/views/forminsc.blade.php
+++ RoboticsCodeRaulWeb/resources/views/forminsc.blade.php
@@ -190,220 +190,82 @@
                     </div>
                 </div>
             </div>
+            <!-- Formulário de Inscrição End -->
+            <!--
+            <div class="container-fluid bg-dark text-white-50 footer pt-5">
+                <div class="container py-5">
+                    <div class="row g-5">
+                        <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.1s">
+                            <a href="index.html" class="d-inline-block mb-3">
+                                <h1>
+                                    <img src="/resources/img/logo.png" alt="Logo" class="img-fluid"
+                                        style="max-height: 100px;">
+                                </h1>
+                            </a>
+                            <p class="mb-0">Robotics Code Raul: Incentivando a inovação e o aprendizado através da
+                                tecnologia
+                                e
+                                da robótica.</p>
+                        </div>
+                        <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.3s">
+                            <h5 class="text-white mb-4">Entre em Contacto Conosco</h5>
+                            <p><i class="fa fa-map-marker-alt me-3"></i>R. Dom João II 33, 2500-852 Caldas<span
+                                    style="text-indent: 28px; display: inline-block;"> da Rainha</span></p>
+                            <p><i class="fa fa-phone-alt me-3"></i>262 740 560</p>
+                            <p><i class="fa fa-envelope me-3"></i>clube.robotica@aerp.pt</p>
+                            <div class="d-flex pt-2">
+                                <a class="btn btn-outline-light btn-social" href="https://linktr.ee/RoboticsCodeRaul/"
+                                    target="_blank"><i class="fas fa-globe"></i></a>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.5s">
+                            <h5 class="text-white mb-4">Páginas Populares</h5>
+                            <a class="btn btn-link" href="">Formulário de Inscrição</a>
+                            <a class="btn btn-link" href="">Sobre Nós</a>
+                        </div>
+                        <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.7s">
+                            <h5 class="text-white mb-4">Outros Links</h5>
+                            <a class="btn btn-link" href="https://aerp.pt/" target="_blank">Portal da Escola</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container wow fadeIn" data-wow-delay="0.1s">
+                    <div class="copyright">
+                        <div class="row">
+                            <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                                &copy; Robotics Code Raul, Todos os direitos reservados.
 
-            <h2 class="my-4">Experiência e Conhecimentos</h2>
-            <div class="row mb-3">
-                <div class="col-6">
-                    <label>Já participaste de um clube de robótica?</label>
-                    <div class="d-flex align-items-center">
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="participouSim" name="participou"
-                                value="sim" required>
-                            <label class="form-check-label" for="participouSim">Sim</label>
-                        </div>
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="participouNao" name="participou"
-                                value="nao" required>
-                            <label class="form-check-label" for="participouNao">Não</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <label>Eu considero as minhas habilidades de programação:</label>
-                    <div class="d-flex align-items-center">
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="habilidadesBoas" name="habilidades"
-                                value="boas" required>
-                            <label class="form-check-label" for="habilidadesBoas">Boas</label>
-                        </div>
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="habilidadesMedia" name="habilidades"
-                                value="media" required>
-                            <label class="form-check-label" for="habilidadesMedia">Média</label>
-                        </div>
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="habilidadesDesenvolver"
-                                name="habilidades" value="desenvolver" required>
-                            <label class="form-check-label" for="habilidadesDesenvolver">Gostaria de me desenvolver
-                                mais nesse ramo</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row mb-3">
-                <div class="col-6">
-                    <label>Tem interesse em eletrónica?</label>
-                    <div class="d-flex align-items-center">
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="interesseSim" name="interesse"
-                                value="sim" required>
-                            <label class="form-check-label" for="interesseSim">Sim</label>
-                        </div>
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="interesseNao" name="interesse"
-                                value="nao" required>
-                            <label class="form-check-label" for="interesseNao">Não</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <label>Você já trabalhou em algum projeto de robótica?</label>
-                    <div class="d-flex align-items-center">
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="projetoSim" name="projeto"
-                                value="sim" required>
-                            <label class="form-check-label" for="projetoSim">Sim</label>
-                        </div>
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="projetoNao" name="projeto"
-                                value="nao" required>
-                            <label class="form-check-label" for="projetoNao">Não</label>
+                                Projetado por Tiago Almeida e Luís Fernandes. Distribuido por
+                                <a class="border-bottom" href="https://aerp.pt" target="_blank">AERP</a>
+                            </div>
+                            <div class="col-md-6 text-center text-md-end">
+                                <div class="footer-menu">
+                                    <a href="/resources/views/index.blade.php">Página Inicial</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
+-->
 
-            <div class="row mb-3">
-                <div class="col-6">
-                    <label>Tem experiência com design e impressão 3D?</label>
-                    <div class="d-flex align-items-center">
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="designSim" name="design"
-                                value="sim" required>
-                            <label class="form-check-label" for="designSim">Sim, ALGUMA</label>
-                        </div>
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="designNao" name="design"
-                                value="nao" required>
-                            <label class="form-check-label" for="designNao">Não</label>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <label>Tem experiência em trabalho em equipa?</label>
-                    <div class="d-flex align-items-center">
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="equipeSim" name="equipe"
-                                value="sim" required>
-                            <label class="form-check-label" for="equipeSim">Sim</label>
-                        </div>
-                        <div class="form-check me-3">
-                            <input class="form-check-input" type="radio" id="equipeNao" name="equipe"
-                                value="nao" required>
-                            <label class="form-check-label" for="equipeNao">Não</label>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <h2 class="my-4">Parte Social Emocional</h2>
-            <div class="mb-3">
-                <label for="comunicacao" class="form-label">Qual a importância da comunicação aberta num
-                    clube?</label>
-                <textarea class="form-control" id="comunicacao" rows="3" required></textarea>
-            </div>
-            <div class="mb-3">
-                <label for="conflitos" class="form-label">Como lidas com conflitos dentro da equipa?</label>
-                <textarea class="form-control" id="conflitos" rows="3" required></textarea>
-            </div>
-            <div class="mb-3">
-                <label for="beneficio" class="form-label">Indique-nos um benefício de trabalhar em
-                    equipa</label>
-                <textarea class="form-control" id="beneficio" rows="3" required></textarea>
-            </div>
-            <div class="mb-3">
-                <label for="ambiente" class="form-label">O que promove um bom ambiente de trabalho positivo
-                    numa
-                    equipa?</label>
-                <textarea class="form-control" id="ambiente" rows="3" required></textarea>
-            </div>
-
-            <div class="row mb-3">
-                <div class="col-12 text-center">
-                    <button type="reset" class="btn btn-secondary">Limpar</button>
-                    <button type="submit" class="btn btn-primary" id="subbtn">Submeter</button>
-                </div>
-            </div>
-        </form>
-    </div>
-    <!-- Formulário de Inscrição End -->
-
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-white-50 footer pt-5">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.1s">
-                    <a href="index.html" class="d-inline-block mb-3">
-                        <h1>
-                            <img src="/resources/img/logo.png" alt="Logo" class="img-fluid"
-                                style="max-height: 100px;">
-                        </h1>
-                    </a>
-                    <p class="mb-0">Robotics Code Raul: Incentivando a inovação e o aprendizado através da tecnologia
-                        e
-                        da robótica.</p>
-                </div>
-                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.3s">
-                    <h5 class="text-white mb-4">Entre em Contacto Conosco</h5>
-                    <p><i class="fa fa-map-marker-alt me-3"></i>R. Dom João II 33, 2500-852 Caldas<span
-                            style="text-indent: 28px; display: inline-block;"> da Rainha</span></p>
-                    <p><i class="fa fa-phone-alt me-3"></i>262 740 560</p>
-                    <p><i class="fa fa-envelope me-3"></i>clube.robotica@aerp.pt</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social" href="https://linktr.ee/RoboticsCodeRaul/"
-                            target="_blank"><i class="fas fa-globe"></i></a>
-                    </div>
-                </div>
-                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.5s">
-                    <h5 class="text-white mb-4">Páginas Populares</h5>
-                    <a class="btn btn-link" href="">Formulário de Inscrição</a>
-                    <a class="btn btn-link" href="">Sobre Nós</a>
-                </div>
-                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.7s">
-                    <h5 class="text-white mb-4">Outros Links</h5>
-                    <a class="btn btn-link" href="https://aerp.pt/" target="_blank">Portal da Escola</a>
-                </div>
-            </div>
-        </div>
-        <div class="container wow fadeIn" data-wow-delay="0.1s">
-            <div class="copyright">
-                <div class="row">
-                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; Robotics Code Raul, Todos os direitos reservados.
-
-                        Projetado por Tiago Almeida e Luís Fernandes. Distribuido por
-                        <a class="border-bottom" href="https://aerp.pt" target="_blank">AERP</a>
-                    </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <div class="footer-menu">
-                            <a href="/resources/views/index.blade.php">Página Inicial</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Footer End -->
+            <!-- Back to Top -->
+            <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top pt-2"><i
+                    class="bi bi-arrow-up"></i></a>
 
 
-    <!-- Back to Top -->
-    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top pt-2"><i
-            class="bi bi-arrow-up"></i></a>
+            <!-- JavaScript Libraries -->
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+            <script src="/resources/lib/wow/wow.min.js"></script>
+            <script src="/resources/lib/easing/easing.min.js"></script>
+            <script src="/resources/lib/wow/wow.min.js"></script>
+            <script src="/resources/lib/owlcarousel/owl.carousel.min.js""></script>
 
-
-    <!-- JavaScript Libraries -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="/resources/lib/wow/wow.min.js"></script>
-    <script src="/resources/lib/easing/easing.min.js"></script>
-    <script src="/resources/lib/wow/wow.min.js"></script>
-    <script src="/resources/lib/owlcarousel/owl.carousel.min.js""></script>
-
-    <!-- Template Javascript -->
-    <script src="/resources/js/main.js"></script>
-    <script src="/resources/js/forminsc.js"></script>
-    <script src="/resources/js/datainsc.js"></script>
+            <!-- Template Javascript -->
+            <script src="/resources/js/main.js"></script>
+            <script src="/resources/js/forminsc.js"></script>
+            <script src="/resources/js/datainsc.js"></script>
 </body>
 
 </html>
